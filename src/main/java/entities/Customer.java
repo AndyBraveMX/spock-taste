a7d42191672e2102fcb680b37a79cec97062513b
andy bravo <andres.bravo@propiedades.com>
2018-01-03 16:43:46 -0600

    Firsr attempt to run spock test for java project

---
+++ src/main/java/entities/Customer.java
@@ -0,0 +1,36 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private String firstName;
+
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
---
+++ src/test/groovy/spock/tests/UnitTesta.java
@@ -0,0 +1,5 @@
+package spock.tests;
+
+public interface UnitTesta {
+
+}
